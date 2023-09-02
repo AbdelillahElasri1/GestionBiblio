3161c9a825176c97d148a87d8689f960859b1cab
elasri <elasriabdelilah68@gmail.com>
2023-09-02 14:23:18 +0100

    first commit

---
+++ GestionDeBibliotheque/src/main/java/utils/InputReader.java
@@ -0,0 +1,12 @@
+package utils;
+
+import java.util.Scanner;
+
+public class InputReader {
+	private final Scanner scanner;
+	
+	public InputReader(java.io.InputStream inputStream) {
+		this.scanner = new Scanner(inputStream);
+	}
+	
+}
